ee0c6c639cfdcfab234a6b3b7a0e95a563277ee2
jmd5439 <jamesmd1996@yahoo.com>
2025-05-21 17:02:12 -0400

    remove webhook

--- netlify/functions/stripe-webhook.js
+++ netlify/functions/stripe-webhook.js
@@ -1,27 +1,2 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-
-exports.handler = async function(event) {
-  const sig = event.headers['stripe-signature'];
-  let stripeEvent;
-
-  try {
-    stripeEvent = stripe.webhooks.constructEvent(
-      event.body,
-      sig,
-      process.env.STRIPE_WEBHOOK_SECRET // Set this in Netlify env vars
-    );
-  } catch (err) {
-    return { statusCode: 400, body: `Webhook Error: ${err.message}` };
-  }
-
-  // Handle the event
-  if (stripeEvent.type === 'checkout.session.completed') {
-    const session = stripeEvent.data.object;
-    // TODO: Notify trainer, update DB, send email, etc.
-  }
-  if (stripeEvent.type === 'checkout.session.expired') {
-    // TODO: Handle canceled/expired payment
-  }
-
-  return { statusCode: 200, body: 'Received' };
-};
+// (You can delete this file or leave it unused.)
+// ...no code needed...
